2c97e39d0996300ab90b3f9d8928ef5e188c4390
Azeem Ghumman <azeemghumman3@gmail.com>
2018-09-30 17:55:59 -0700

    create folder structure

---
+++ scripts/run.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+
+print ("run main loop")
+print ("expect a manifest file name as input")
---
+++ scripts/validate.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+
+print ("validate script")
+print ("expect a manifest file name as input and check if the manifest file makes sense. yaml errors. connections, etc")
+print ("This could be used for diagnostic purposes")
